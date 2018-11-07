f41189d03a420902ac1e3b2fc05d372632b07149
clawrence <clawrence@acceptanceinsurance.com>
2018-11-07 08:06:42 -0600

    Styling changes

--- AspnetVue/src/App.vue
+++ AspnetVue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar app dark color="primary">
+    <v-toolbar app dark>
       <v-toolbar-title class="headline text-uppercase">
         <span>Acceptance</span> &nbsp;
         <span class="font-weight-light">ASPNETCORE/VUE DEMO</span>
