1b99bd27a7d0d4222029feaf1f40316ba18c9222
clawrence <clawrence@acceptanceinsurance.com>
2018-12-11 15:06:41 -0600

    Converting App.vue to TS

--- AspnetVue/src/App.vue
+++ AspnetVue/src/App.vue
@@ -20,47 +20,57 @@
   </v-app>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
 import HelloWorld from './components/HelloWorld.vue'
 import Alerts from './components/Alerts.vue';
-import alertMixin  from './shared/alert-mixin.ts'
-import progressBarMixin  from './shared/progress-bar-mixin.ts';
+import alertMixin  from './shared/alert-mixin'
+import progressBarMixin  from './shared/progress-bar-mixin';
 import ProgressBar from './components/ProgressBar.vue';
-import DataServices from './services/data-services.ts';
+import DataServices from './services/data-services';
 
-export default {
-  name: 'App',
+@Component({  
+  components: {
+    HelloWorld,
+    Alerts,
+    ProgressBar,
+  }
+})
+
+export default class App extends Vue {
+
   created() {
     const urlParams = new URLSearchParams(window.location.search);
     const sessionId = urlParams.get('sessionId');
+    
+    console.log('App created')
     if (sessionId === null) {
+    
+      console.log('SessionId is null')
       //TODO: This does not work
       alertMixin.error('Something is wrong with the SessionId.');
       return;
     }
-
-    //This works
+            
+    //TODO: This does not work
     progressBarMixin.toggle();
 
     DataServices.getUserSessionData(sessionId)
       .then((result) => {
         console.log(result)
-        //This works
+        // This works
         alertMixin.success('Your sessionId is valid');
       })
       .catch((err) => {
         alertMixin.error(err); 
       })
-      .finally(() => {        
-        //TODO: This does not work
-        progressBarMixin.toggle();
+      .finally(() => {
+        // This works
+        //progressBarMixin.toggle();
       });
-  },
-  components: {
-    HelloWorld,
-    Alerts,
-    ProgressBar,
-  },
-  mixins: [alertMixin, progressBarMixin]
+  };
+  
+  //mixins: [alertMixin, progressBarMixin];
 }
 </script>
